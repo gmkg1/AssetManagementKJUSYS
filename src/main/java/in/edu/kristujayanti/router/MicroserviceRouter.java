0c10b64f895a08e1972f7c126b831af62cd0e6a9
gmkg1 <maheengmk@gmail.com>
2026-06-17 13:02:02 +0530

    UNITLIST

--- src/main/java/in/edu/kristujayanti/router/MicroserviceRouter.java
+++ src/main/java/in/edu/kristujayanti/router/MicroserviceRouter.java
@@ -183,38 +183,6 @@ public class MicroserviceRouter extends RouterBase {
         HttpMethod.GET,
         MicroserviceRoutingURLNames.GET_ASSET_HISTORY,
         new GetAssetHistoryHandler(assetsService));
-
-    addRoute(
-        HttpMethod.GET,
-
-        MicroserviceRoutingURLNames.UNITS_LIST,
-        new GetUnitsListHandler(assetsService));
-
-    addRoute(
-            HttpMethod.GET,
-        MicroserviceRoutingURLNames.EXPORT_REPORTS,
-        new ExportReportsHandler(assetsService));
-
-    addRoute(
-        HttpMethod.GET,
-        MicroserviceRoutingURLNames.REPORTS_GROUPED,
-        new GroupedReportsHandler(assetsService));
-
-    addRoute(
-        HttpMethod.GET,
-        MicroserviceRoutingURLNames.EXPORT_ASSETS,
-        new ExportAssetsHandler(assetsService));
-
-    addRoute(
-        HttpMethod.GET,
-        MicroserviceRoutingURLNames.EXPORT_ISSUE_LOGS,
-        new ExportIssueLogsHandler(assetsService));
-
-    addRoute(
-        HttpMethod.GET,
-        MicroserviceRoutingURLNames.EXPORT_RETURN_LOGS,
-        new ExportReturnLogsHandler(assetsService));
-
   }
 
 
